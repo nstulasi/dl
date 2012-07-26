014376029e778756014723dea46efc5ffa9c0f33
Sai <tulasi.neppali@gmail.com>
2012-07-26 05:06:46 -0400

    changes

--- db/migrate/20120712200719_add_start_and_end_date_to_project.rb
+++ db/migrate/20120712200719_add_start_and_end_date_to_project.rb
@@ -2,6 +2,6 @@ class AddStartAndEndDateToProject < ActiveRecord::Migration
   def change
     add_column :projects, :start_at, :date
     add_column :projects, :end_at, :date
-    remove_column :projects, :duration
+    remove_column :projects, :time
   end
 end
